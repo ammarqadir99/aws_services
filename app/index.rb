ccb0d33509c10088e52642ed084d960f2563eca9
Ammar Qadir <52775478+ammarqadir99@users.noreply.github.com>
2023-11-21 02:27:31 +0500

    changes to lambda & build.yml

--- app/index.rb
+++ app/index.rb
@@ -3,5 +3,5 @@ require 'json'
 require 'logger'
 
 def handler(event:, context:)
-    { statusCode: 200, body: JSON.generate('update naveed LAMBDA WITH GITHUB ACITONSa !') }
+    { statusCode: 200, body: JSON.generate('updated naveed LAMBDA WITH GITHUB ACITONSa !') }
 end
